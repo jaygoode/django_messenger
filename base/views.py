bc55fb42e1e75d8a153b4256b3267e93a8000035
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-09-10 21:45:24 +0300

    added some models for db, created superuser, dynamic room urls for rooms in db

--- base/views.py
+++ base/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+
+# rooms = [
+#     {"id": 1, "name": "Lets learn python"},
+#     {"id": 2, "name": "Lets learn java"},
+#     {"id": 3, "name": "Lets learn go"},
+# ]
 
 
 def home(request):
-    return HttpResponse("Home page")
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return HttpResponse("ROOM")
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, "base/room.html", context)
